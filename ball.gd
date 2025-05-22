a613e1ba50ea6737d435c486ad5e165691da2735
Aries Magat <aries.magat27@gmail.com>
2025-05-22 18:41:30 -0400

    Initial Commit for bick game on Godot

---
+++ ball.gd
@@ -0,0 +1,56 @@
+extends RigidBody2D
+
+@export var speed := 400
+var screen_size
+var is_resetting
+var reset_target_position := Vector2.ZERO
+var reset_target_velocity := Vector2.ZERO
+var is_in_Narnia:bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	linear_velocity = Vector2(randf_range(0.0,1.0),-randf_range(0.0,0.6))
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if is_resetting:
+		#state.transform is the full 2D transform of the body: its position, rotation, and scale.
+		state.transform.origin = reset_target_position
+		#“Hey physics system, here’s your updated truth. Forget whatever momentum you had — this is where the body is now.”
+		state.linear_velocity = reset_target_velocity
+		return
+
+	if linear_velocity.length() != speed:
+		linear_velocity = linear_velocity.normalized() * speed
+		if is_in_Narnia:
+			print("pos: ", position)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node) -> void:
+	print("entered ", body.name)
+	if body.name == "WallBot":
+		is_in_Narnia = false
+		_on_lost()
+	elif body.name == "WallTop":
+		print("something happened")
+		is_in_Narnia = true
+		print("position ", position)
+	##elif body.name == "Brick": ##do something here
+	else:
+		is_in_Narnia = false
+	
+func _on_lost() -> void:
+	##save score
+	is_resetting = true
+	reset_target_position = Vector2(135, 420)
+	reset_target_velocity = Vector2(linear_velocity)
+	hide()
+	await get_tree().create_timer(0.8).timeout
+	show()
+	await get_tree().process_frame
+	await get_tree().process_frame
+	is_resetting = false
+	pass
